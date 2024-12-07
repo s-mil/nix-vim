952f52acc46fb35dd68c852e92a52e49cb5be0ba
Sam Miller <23262411+s-mil@users.noreply.github.com>
2024-12-07 11:25:44 -0600

    Update dotnet for neovim

---
+++ config/plugins/lang/dotnet.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+{
+  plugins = {
+    conform-nvim.settings = {
+      formatters_by_ft = {
+        csharp = [ "csharpier" ];
+      };
+
+      formatters = {
+        csharpier = {
+          command = "${pkgs.csharpier}/bin/dotnet-csharpier";
+        };
+      };
+    };
+
+    lsp.servers = {
+      omnisharp.enable = true;
+
+      extraOptions = {
+        init_options = {
+          usePlaceholders = true;
+          completeUnimported = true;
+        };
+      };
+    };
+  };
+
+  dap = {
+    adapters.executables.lldb.command = "${pkgs.lldb}/bin/lldb-vscode";
+
+    configurations.csharp = [
+      {
+        name = "C# Debug";
+        type = "lldb";
+        request = "launch";
+        cwd = "\${workspaceFolder}";
+        program.__raw = ''
+          function()
+            return vim.fn.input('Executable path: ', vim.fn.getcwd() .. '/', 'file')
+          end
+        '';
+      }
+    ];
+  };
+}
